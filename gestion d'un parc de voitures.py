6fa9c15c46dc3ec6cb5a29d8a5f2b864a15fa898
Rayan Lounis <lounisrayan200@gmail.com>
2026-03-08 22:43:36 -0400

    Ajout structure classe Parc

--- gestion d'un parc de voitures.py
+++ gestion d'un parc de voitures.py
@@ -2,6 +2,13 @@ class Voiture:
     def __init__(self,marque,matricule,couleur):
         self.marque = marque
         self.matricule = matricule
-        self.couleur = coleur
+        self.couleur = couleur
     def afficheriInformations(self):
         print(self.marque,self.matricule,self.couleur)
+class Parc:
+    def __init__(self,identifiant,adresse,capacite):
+        self.identifiant = identifiant
+        self.adresse = adresse
+        self.capacite = capacite
+        self.liste = []
+
